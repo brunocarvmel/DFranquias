58147dd107fa811f7c1eafbbd8ec50ac7ee4e96f
bruno carvalho <brunno.carvalho.melo@gmail.com>
2023-10-06 16:20:29 -0300

    feat: adicionando a camada de serviços

--- app/Http/Controllers/GadoController.php
+++ app/Http/Controllers/GadoController.php
@@ -4,109 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Abate;
 use App\Models\Gado;
+use App\Services\GadoService;
 use Illuminate\Http\Request;
 
 class GadoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
 
+    private $gadoService;
 
+    public function __construct(){
+        $this->gadoService = new GadoService();
+    }
 
     public function index()
     {
         return response()->json(
-            Gado::all()
+            $this->gadoService->index()
         );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        try {
-            $gado = Gado::create([
-                'codigo'=>5345212,
-                'qtd_leite'=>12.3,
-                'qtd_racao'=>12.3,
-                'peso'=>12.3,
-                'nascimento'=>'2023-10-04'
-            ]);
-
-            return response()->json([
-                'success' => true,
-                'data' => $gado
-            ]);
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Não foi possível salvar o gado! Verifique as informações e tente novamente',
-                'data' => []
-            ]);
-        }
+       return response()->json(
+        $this->gadoService->store($request)
+       );
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
-    {
-        $gado = Gado::find($id);
-
-        if (empty($gado)) {
-            return response()->json([
-                'message' => 'Record not found.'
-            ], 404);
-        }
-
+    {   
         return response()->json(
-            $gado
+            $this->gadoService->show($id)
         );
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        $gado = Gado::find($id);
-
-        if (empty($gado)) {
-            return response()->json([
-                'message'=>'Record not found.',
-            ], 404);
-        }
-
-        $gado->update(
-            $request->all()
+        return response()->json(
+            $this->gadoService->update($request, $id)
         );
-        return response()->json([
-            'succes' => true,
-            'data' => $gado
-        ]);
-
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {   
-        $gado = Gado::find($id);
-        if (empty($gado)) {
-            return response()->json([
-                'message'=>'Record not found.',
-            ], 404);
-        }
-
-        $gado->delete();
-        return response()->json([
-            'success'=>true,
-            'data'=>$gado
-        ]);
+            return $this->gadoService->destroy($id);
     }
 }
---
+++ app/Services/GadoService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Gado;
+
+class GadoService extends Service {
+
+    public function index(){
+        return Gado::all();
+    }
+
+    public function store($request){
+        try {
+            $gado = Gado::create(
+                $request->all()
+            );
+
+            return [
+                'success'=>true,
+                'data'=>$gado
+            ];
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Não foi possível salvar o gado! Verifique as informações e tente novamente',
+                'data' => []
+            ], 404);
+        }
+    }
+
+    public function show($id){
+        $gado = Gado::find($id);
+
+        if (empty($gado)) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        }
+
+        return response()->json(
+            $gado
+        );
+    }
+
+    public function update($request, $id){
+        $gado = Gado::find($id);
+
+        if (empty($gado)) {
+            return response()->json([
+                'message'=>'Record not found.',
+            ], 404);
+        }
+
+        $gado->update(
+            $request->all()
+        );
+        return response()->json([
+            'succes' => true,
+            'data' => $gado
+        ]);
+    }
+
+    public function destroy($id){
+        $gado = Gado::find($id);
+        if (empty($gado)) {
+            return response()->json([
+                'message'=>'Record not found.',
+            ], 404);
+        }
+
+        $gado->delete();
+        return response()->json([
+            'success'=>true,
+            'data'=>$gado
+        ]);
+    }
+}
